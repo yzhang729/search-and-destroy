9665b4dabb9502c3a1f3c54ec5569fe50a85da9d
Yuting Zhang <yutingzhang729@gmail.com>
2019-07-19 12:19:13 -0400

    finished exercises

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,26 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  //start at first element
+  //look for largest element within range
+  //iterate until you reach end
+  let jumpCount = 0;
+  let i = 0;
+  while (i < arr.length) {
+    let currentArr = arr.slice(i + 1, i + arr[i] + 1);
+    let maxValue = currentArr.reduce((a, b) => (a < b ? b : a));
+    if (i + arr[i] >= arr.length - 1) {
+      jumpCount++;
+      return jumpCount;
+    }
+    if (i + maxValue >= arr.length - 1) {
+      jumpCount += 2;
+      return jumpCount;
+    }
+    let maxValueIndex = arr.indexOf(maxValue);
+    i = maxValueIndex;
+    jumpCount++;
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
--- min-jumps/min-jumps.spec.js
+++ min-jumps/min-jumps.spec.js
@@ -1,22 +1,21 @@
 'use strict';
-const {expect} = require('chai');
+const { expect } = require('chai');
 const minJumps = require('./min-jumps');
 
-const test1 = [1,1];
-const test2 = [6,3,2,1];
-const test3 = [2,8,4,3,2,9,6,8];
-const test4 = [4,4,2,7,1,1,1,1,3,7,2];
-const test5 = [2,4,1,1,2,3,7,1,1,3];
-
+const test1 = [1, 1];
+const test2 = [6, 3, 2, 1];
+const test3 = [2, 8, 4, 3, 2, 9, 6, 8];
+const test4 = [4, 4, 2, 7, 1, 1, 1, 1, 3, 7, 2];
+const test5 = [2, 4, 1, 1, 2, 3, 7, 1, 1, 3];
 
 describe('min number of jumps', () => {
-	it('returns the min # of jumps to cross an array of positive integers', () => {
-		expect(minJumps(test1)).to.equal(1);
-		expect(minJumps(test2)).to.equal(1);
-		expect(minJumps(test3)).to.equal(2);
-		expect(minJumps(test4)).to.equal(2);
-		expect(minJumps(test5)).to.equal(4);
-		
-		//Feel free to write your own tests. Include >=1 edge case :)
-	});
+  it('returns the min # of jumps to cross an array of positive integers', () => {
+    expect(minJumps(test1)).to.equal(1);
+    expect(minJumps(test2)).to.equal(1);
+    expect(minJumps(test3)).to.equal(2);
+    expect(minJumps(test4)).to.equal(2);
+    expect(minJumps(test5)).to.equal(4);
+
+    //Feel free to write your own tests. Include >=1 edge case :)
+  });
 });
