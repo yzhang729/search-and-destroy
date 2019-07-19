699d6625e02e42711aa162e4eb5c478e7a663432
Yuting Zhang <yutingzhang729@gmail.com>
2019-07-19 11:21:12 -0400

    working on extra credit for 1

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,25 +1,25 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
+const binarySearch = (array, target, head = 0, tail = array.length - 1) => {
   //if array length is 1, return true or false depending on value
   //find midpoint of array
   //if midpoint matches target, return true
   //else compare target to midpoint
   //recursively perform binarySearch on left/right subarray
-  if (array.length === 1) {
-    return array[0] === target;
+  if (tail - head === 0) {
+    return array[head] === target;
   }
 
-  let midpoint = Math.floor(array.length / 2);
+  let midpoint = Math.floor((tail + 1 - head) / 2) - 1;
   if (array[midpoint] === target) {
     return true;
   } else if (array[midpoint] > target) {
-    const leftArray = array.slice(0, midpoint);
-    return binarySearch(leftArray, target);
+    //const leftArray = array.slice(0, midpoint);
+    return binarySearch(array, target, head, midpoint - 1);
   } else {
-    const rightArray = array.slice(midpoint);
-    return binarySearch(rightArray, target);
+    //const rightArray = array.slice(midpoint);
+    return binarySearch(array, target, midpoint + 1);
   }
 };
 
